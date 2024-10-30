6a4e2bdbd72b02e80492d4008e64d191694e8a21
piercearnold-pvcc <pa67092@email.vccs.edu>
2024-10-30 16:21:10 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#Name: Pierce Arnold
+#Prog Purpose: Hello World
+
+print("Hello World! This is my first python program.")
+print("I can program in HTML, which is awesome.")
+print("However, HTML cannot do arithmatic.")
+print("But Python can: \n\t Add  \n\t Subtract \n\t Multiply \n\t Devide")
+print("And lots of other things!")
